47afab70b45883f0a5dfe6df9591ed24b91a775d
RkzJacob <jaco.ponce@duocuc.cl>
2022-04-30 17:24:43 -0400

    urls y views
    Agregado de urls y paginas html para el uso de las mismas

--- ProyectoWeb/AppWeb/urls.py
+++ ProyectoWeb/AppWeb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import home
+from .views import home,panelAdmin,panelMedico,panelBodeguero,panelFarmaceutico,caducarMedicamentos,eliminarCuentas,generarInformes,iniciarSesion,modificarCuentas,ConsultarMedicamentos,registrarConsulta,registrarCuentas,registrarMedicamentos,reservarMedicamentos,retiroMedicamentos,revisarStock,verCuentas
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +9,9 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('',home,name='home'),
+    path('Panel_medico/',panelMedico,name='panelMedico'),
+    path('Panel_bodeguero/',panelBodeguero,name='panelBodeguero'),
+    path('Panel_farmaceutico/',panelFarmaceutico,name='panelFarmaceutico'),
 
 
 ]
--- ProyectoWeb/AppWeb/views.py
+++ ProyectoWeb/AppWeb/views.py
@@ -1,6 +1,69 @@
 from django.shortcuts import render,redirect
 
 
-
+#Pag home
 def home(request):
     return render(request,'AppWeb/home.html')
+#Pag iniciar sesión
+def iniciarSesion(request):
+    return render(request,'AppWeb/iniciar sesion.html')
+
+#Pag panel medico
+def panelMedico(request):
+    return render(request,'AppWeb/panel medico.html')
+    #Descompocisión panel medico #Pag registrar consulta medica
+def registrarConsulta(request):
+    return render(request,'AppWeb/RegistrarConsulta.html') 
+    #Descompocisión panel medico #Pag revisar stock de medicamentos
+def revisarStock(request):
+    return render(request,'AppWeb/revisar stock.html') 
+
+
+#Pag panel bodeguero
+def panelBodeguero(request):
+    return render(request,'AppWeb/panel bodeguero.html')
+    #Descompocisión panel bodeguero #Pag registrar medicamentos
+def registrarMedicamentos(request):
+    return render(request,'AppWeb/registrarMedicamento.html')
+    #Descompocisión panel bodeguero #Pag Caducar medicamentos
+#def caducarMedicamentos(request):
+    #return render(request,'AppWeb/caducarMedicamento.html')
+
+
+#Pag panel farmaceutico
+def panelFarmaceutico(request):
+    return render(request,'AppWeb/panel farmaceutico.html')
+    #Descompocisión panel farmaceutico #Pag Caducar medicamentos
+def caducarMedicamentos(request):
+    return render(request,'AppWeb/caducarMedicamento.html')
+    #Descompocisión panel farmaceutico #Pag consultar medicamentos
+def ConsultarMedicamentos(request):
+    return render(request,'AppWeb/consultar medicamentos.html')
+    #Descompocisión panel farmaceutico #Pag reservar medicamentos
+def reservarMedicamentos(request):
+    return render(request,'AppWeb/reservarMedicamentos.html')
+    #Descompocisión panel farmaceutico #Pag retiro de medicamentos
+def retiroMedicamentos(request):
+    return render(request,'AppWeb/retiroMedicamentos.html')
+
+
+#Pag panel admin
+def panelAdmin(request):
+    return render(request,'AppWeb/panel admin.html')
+    #Descompocisión panel admin #Pag Registrar cuentas de usuario
+def registrarCuentas(request):
+    return render(request,'AppWeb/registrar cuentas.html')
+    #Descompocisión panel admin #Pag modificar cuentas de usuario
+def modificarCuentas(request):
+    return render(request,'AppWeb/modificar cuentas.html')
+    #Descompocisión panel admin #Pag eliminar cuentas de usuario
+def eliminarCuentas(request):
+    return render(request,'AppWeb/eliminar cuentas.html')
+    #Descompocisión panel admin #Pag generar informes
+def generarInformes(request):
+    return render(request,'AppWeb/generacionInformes.html')
+    #Descompocisión panel admin #Pag ver cuentas del sistema
+def verCuentas(request):
+    return render(request,'AppWeb/ver cuentas.html')
+
+
