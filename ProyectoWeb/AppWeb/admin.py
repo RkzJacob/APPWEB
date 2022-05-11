bc1875fd5de9d64466d073fafec99bab681ba2d8
RkzJacob <jaco.ponce@duocuc.cl>
2022-05-10 21:54:46 -0400

    pagina de retiro de medicamentos
    pagina de medicamentos semi hecha

--- ProyectoWeb/AppWeb/admin.py
+++ ProyectoWeb/AppWeb/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Medicamento,Consulta
+from .models import Medicamento,Consulta,Retiro
 
 # Register your models here.
 admin.site.register(Medicamento)
 admin.site.register(Consulta) 
+admin.site.register(Retiro) 
